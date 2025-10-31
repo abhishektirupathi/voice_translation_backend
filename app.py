114ec30614725e1e6d9dba2543d796986c4d61a5
Abhishek Tirupathi <tirupathiabhi123@gmail.com>
2025-10-31 18:49:57 +0530

    Prepare for Render: use PORT and add gunicorn

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def delete_single_translation(translation_id):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('DEBUG', 'False').lower() == 'true'
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=debug)
